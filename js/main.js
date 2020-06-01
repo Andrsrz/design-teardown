f19a0036a9a355fa30108b37804fcddc7a2ad9e1
Andres Ruiz <andrsruiz@gmail.com>
2020-06-01 12:40:01 -0500

    Generate old Articles with JS

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const LARGE_TEXT = "Lorem ipsum dolor sit amet, decore dignissim ne usu. Vel omn
 menandri ea. Facer melius eruditi no mei, an semper molestiae eos. At viris tibique adipisci \
 ius, usu an omnes doming dissentiunt. Zril detraxit reformidans his te.";
 const MAX_NEW_ARTICLES = 3;
+const MAX_OLD_ARTICLES = 6;
 
 function generateNewArticles(){
 	let newArticlesDiv = document.getElementById("site-main-new-articles");
@@ -13,7 +14,7 @@ function generateNewArticles(){
 		let article = document.createElement("div");
 		article.setAttribute("class", "new-articles");
 		let photo = document.createElement("div");
-		photo.setAttribute("class", "new-articles-photo");
+		photo.setAttribute("class", "photo");
 		let author = document.createElement("h3");
 		author.innerHTML = MINI_TEXT;
 		let title = document.createElement("h1");
@@ -26,6 +27,24 @@ function generateNewArticles(){
 	}
 }
 
+function generateOldArticles(){
+	let oldArticlesDiv = document.getElementById("site-main-old-articles");
+	/* The old articles are six, so we loop six times to create them */
+	for(let i = 0; i < MAX_OLD_ARTICLES; i++){
+		let article = document.createElement("div");
+		article.setAttribute("class", "old-articles");
+		let title = document.createElement("h2");
+		title.innerHTML = MINI_TEXT;
+		let description = document.createElement("h4");
+		description.innerHTML = SMALL_TEXT;
+
+		article.appendChild(title);
+		article.appendChild(description);
+		oldArticlesDiv.appendChild(article);
+	}
+}
+
 function pageLoaded(){
 	generateNewArticles();
+	generateOldArticles();
 }
